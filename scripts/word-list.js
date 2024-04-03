c028fe43155d0b4f91ddd89356c0b3370da19bd3
Selvam-18 <128716393+Selvam-18@users.noreply.github.com>
2024-04-03 11:14:01 -0700

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,82 @@
+const wordList = [
+    {
+        word:"cpu",
+        hint:"The brain of the computer."
+    },
+    {
+        word:"bit",
+        hint:"The smallest unit of memory."
+    },
+    {
+        word:"machineleanring",
+        hint:"Language writtern in binary form"
+    },
+    {
+        word:"computer",
+        hint:"Device that cannot be carried out."
+    },
+    {
+        word:"modem",
+        hint:"Device needed by computer to connect to the internet."
+    },
+    {
+        word:"spacebar",
+        hint:"Biggest key in keyboard."
+    },
+    {
+        word:"windows",
+        hint:"Popular operating system developed my Microsoft."
+    },
+    {
+        word:"python",
+        hint:"A high-level programming langugage."
+    },
+    {
+        word:"monitor",
+        hint:"Top most output device"
+    },
+    {
+        word:"bug",
+        hint:"Faults in computer program."
+    },
+    {
+        word:"ram",
+        hint:"temporary memory storage."
+    },
+    {
+        word:"google",
+        hint:"Top first search engine."
+    },
+    {
+        word:"cookie",
+        hint:"Saves the information of user temporarily."
+    },
+    {
+        word:"email",
+        hint:"Digital versions of letters."
+    },
+    {
+        word:"recyclebin",
+        hint:"Temporary storage for deleted files."
+    },
+    {
+        word:"variable",
+        hint:"Container to store values."
+    },
+    {
+        word:"internet",
+        hint:"Collection of inter connected networks."
+    },
+    {
+        word:"charlesbabbage",
+        hint:"The father of computer."
+    },
+    {
+        word:"program",
+        hint:"Set of instructions to specify a work."
+    },
+    {
+        word:"drive",
+        hint:"A cloud storage."
+    }
+];
